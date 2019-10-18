a8971cac7c70cb9b7c7245e91d71e18451e7acdf
Daniel Patynski <daniel.patynski@amido.com>
2019-10-18 15:34:26 +0100

    Initial setup with the local static repository of jars.

---
+++ src/main/java/uk/gov/hmcts/reform/idam/notify/client/SmsGatewayImpl.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.idam.notify.client;
+
+
+import com.sun.identity.authentication.modules.hotp.SMSGateway;
+
+import java.util.Map;
+
+public class SmsGatewayImpl implements SMSGateway {
+
+    public void sendSMSMessage(
+            String s, String s1, String s2, String s3, String s4, Map map) throws AuthLoginException {
+
+    }
+
+    public void sendEmail(String s, String s1, String s2, String s3, String s4, Map map) throws AuthLoginException {
+
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/idam/notify/client/SmsGatewayImplTest.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.idam.notify.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SmsGatewayImplTest {
+
+    @Test
+    public void sendSMSMessage() {
+    }
+
+    @Test
+    public void sendEmail() {
+    }
+}
