409924c427e42b55be79c2f34c30c75bc4a139a1
Daniel Patynski <daniel.patynski@amido.com>
2019-10-18 15:54:18 +0100

    Add openam-core-5.5.1-1.0.0.

--- src/main/java/uk/gov/hmcts/reform/idam/notify/client/SmsGatewayImpl.java
+++ src/main/java/uk/gov/hmcts/reform/idam/notify/client/SmsGatewayImpl.java
@@ -2,16 +2,19 @@ package uk.gov.hmcts.reform.idam.notify.client;
 
 
 import com.sun.identity.authentication.modules.hotp.SMSGateway;
+import com.sun.identity.authentication.spi.AuthLoginException;
 
 import java.util.Map;
 
 public class SmsGatewayImpl implements SMSGateway {
 
+    @Override
     public void sendSMSMessage(
             String s, String s1, String s2, String s3, String s4, Map map) throws AuthLoginException {
 
     }
 
+    @Override
     public void sendEmail(String s, String s1, String s2, String s3, String s4, Map map) throws AuthLoginException {
 
     }
